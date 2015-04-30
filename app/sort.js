e92dfef347e96efa23a65e4c7d59cc7e6a6408e3
plsr <christianpoplawski@gmail.com>
2015-04-30 12:00:29 +0200

    Exercise 3 Skyscrapers are sorted and written to a new json file

--- app/sort.js
+++ app/sort.js
@@ -10,13 +10,26 @@ fs.readFile('../io/skyscraper.json', function (err, data) {
   	// Parse json
   	var contents = JSON.parse(data);
 
-  	// Print needed logs for every skyscraper object
-  	contents.wolkenkratzer.forEach(function (curr) {
-  		console.log("Name: " + chalk.blue('%s'), curr.name);
-  		console.log("Stadt: " + chalk.green('%s'), curr.stadt);
-  		console.log("Hoehe: " + chalk.red('%s'), curr.hoehe);
-  		console.log("~~~~~~~~~~~~~~~~~~~~");
+  	// Sort ascending by height
+  	contents.wolkenkratzer.sort(function(a, b) {
+  		return a.hoehe - b.hoehe;
+  	});
+
+  	// Write sorted list to file, then log the objects
+  	fs.writeFile('../io/skyscraper_asc.json', JSON.stringify(contents), function(err) {
+  		if (err) throw err;
+  		console.log("Saverino!");
+
+  		// Print needed logs for every skyscraper object
+  		contents.wolkenkratzer.forEach(function (curr) {
+  			console.log("Name: " + chalk.blue('%s'), curr.name);
+  			console.log("Stadt: " + chalk.green('%s'), curr.stadt);
+  			console.log("Hoehe: " + chalk.red('%s'), curr.hoehe);
+  			console.log("~~~~~~~~~~~~~~~~~~~~");
+  		});
   	})
+
+ 
   	
   }
   
